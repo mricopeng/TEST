bbcc2c74b760509708d6a8824817f13d722f4d8a
mricopeng <marcos@palmier.io>
2024-09-17 16:51:01 -0700

    [feature] Add new jokes to the joke list
    Expand the collection of jokes in the application by adding 10 new jokes to the existing list. This increases the variety of jokes available when the user clicks the "Tell me a joke" button.
    
    Notes: The original application was created to display random jokes when a button is clicked. This update enhances the user experience by providing a wider range of jokes, including some programming-related humor, which aligns with the initial concept of a simple joke-telling application.

--- joke.py
+++ joke.py
@@ -7,6 +7,17 @@ jokes = [
     "Why don't eggs tell jokes? They'd crack each other up!",
     "What do you call a fake noodle? An impasta!",
     "Why did the math book look so sad? Because it had too many problems!",
+    # New jokes
+    "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+    "Why do programmers prefer dark mode? Because light attracts bugs!",
+    "Why did the JavaScript developer quit his job? Because he didn't get arrays.",
+    "How do you comfort a JavaScript bug? You console it!",
+    "Why do Java developers wear glasses? Because they don't C#!",
+    "Why was six afraid of seven? Because seven eight nine!",
+    "What's the best way to watch a fly fishing tournament? Live stream.",
+    "Why did the scarecrow become a successful motivational speaker? He was outstanding in his field.",
+    "What do you call a parade of rabbits hopping backwards? A receding hare-line.",
+    "Why don't scientists trust atoms? Because they make up everything!",
 ]
 
 def tell_joke():
